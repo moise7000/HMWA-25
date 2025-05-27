5d7c5d5e6bfef4f2958e162cdad588754f10a4c9
Xavier Anicito <anicito.xavier@gmail.com>
2025-05-27 23:47:07 +0200

    Fixed links.

--- webapp/components/common/Navbar.vue
+++ webapp/components/common/Navbar.vue
@@ -5,16 +5,16 @@
     </div>
     <div class="space-x-4">
       <NuxtLink to="/" class="text-gray-700 hover:text-blue-600">Home</NuxtLink>
-      <NuxtLink to="/course-and-subscription" class="text-gray-700 hover:text-blue-600">Courses & Subscriptions</NuxtLink>
+      <NuxtLink to="/courses-and-subscriptions" class="text-gray-700 hover:text-blue-600">Courses & Subscriptions</NuxtLink>
       <NuxtLink to="/events" class="text-gray-700 hover:text-blue-600">Events</NuxtLink>
-      <NuxtLink to="/institute" class="text-gray-700 hover:text-blue-600">Institue</NuxtLink>
+      <NuxtLink to="/institute" class="text-gray-700 hover:text-blue-600">Institute</NuxtLink>
       <NuxtLink to="/about" class="text-gray-700 hover:text-blue-600">About Yoga</NuxtLink>
-      <NuxtLink to="/institue/conact" class="text-gray-700 hover:text-blue-600">Contact</NuxtLink>
+      <NuxtLink to="/institute/contact" class="text-gray-700 hover:text-blue-600">Contact</NuxtLink>
       <NuxtLink to="/" class="text-gray-700 hover:text-blue-600">[Empty]]</NuxtLink>
     </div>
   </nav>
 </template>
 
 <script setup>
-// aucun script nécessaire ici
+
 </script>
