942a7980037cd7172361547b29035d6eb6f306a0
moise7000 <77933570+moise7000@users.noreply.github.com>
2025-05-29 11:16:46 +0200

    [+] all teachers page: hero card

--- webapp/pages/institute/teachers/index.vue
+++ webapp/pages/institute/teachers/index.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 
+import HeroCard from "~/layouts/Hero.vue";
 </script>
 
 <template>
-
+  <HeroCard
+      title="Meet the teachers"
+      description="<br>Meet our passionate and experienced yoga teachers, each dedicated to guiding you on your journey with care, knowledge, and inspiration. <br> Discover the heart of our studio through the people who bring it to life."
+  />
 </template>
 
 <style scoped>
 
 </style>
+
