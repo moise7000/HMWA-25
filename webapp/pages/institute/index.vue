81c5d55b5108ed68f99a1e7976c18ff4a72f909b
moise7000 <77933570+moise7000@users.noreply.github.com>
2025-06-04 21:12:39 +0200

    [~]

--- webapp/pages/institute/index.vue
+++ webapp/pages/institute/index.vue
@@ -12,31 +12,31 @@ const description = 'Hello from out institute.'
 
 const topics = [
 	{
-		imagePath : 'assets/yoga_courses/yoga_course_0.png',
+		imagePath : 'assets/yoga_courses/yoga_course_4.png',
 		topicDescription : 'Ceci est un 2eme test.',
 		linkPath : '/institute/story',
 		topicTitle : 'Our story',
 		side : true
 	}, {
-		imagePath : 'assets/yoga_courses/yoga_course_0.png',
+		imagePath : 'assets/yoga_courses/yoga_course_5.png',
 		//topicDescription
 		linkPath : '/institute/teachers',
 		topicTitle : 'Your teachers',
 		side : false
 	}, {
-		imagePath : 'assets/yoga_courses/yoga_course_0.png',
+		imagePath : 'assets/yoga_courses/yoga_course_6.png',
 		//topicDescription
 		linkPath : '/institute/opening-hours',
 		topicTitle : 'Opening hours',
 		side : true
 	}, {
-		imagePath : 'assets/yoga_courses/yoga_course_0.png',
+		imagePath : 'assets/yoga_courses/yoga_course_7.png',
 		//topicDescription
 		linkPath : '/institute/location',
 		topicTitle : 'Our address',
 		side : false
 	}, {
-		imagePath : 'assets/yoga_courses/yoga_course_0.png',
+		imagePath : 'assets/yoga_courses/yoga_course_8.png',
 		//topicDescription
 		linkPath : '/institute/contact',
 		topicTitle : 'Contact us',
