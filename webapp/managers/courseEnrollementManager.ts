95f989ca8e1c5ae64c3fcf95d9aabd4a24cc57c1
moise7000 <77933570+moise7000@users.noreply.github.com>
2025-06-06 19:23:20 +0200

    [+] Enroll in course features

---
+++ webapp/managers/courseEnrollementManager.ts
@@ -0,0 +1,56 @@
+import type { CourseEnrollment } from '~/types/CourseEnrollment'
+import type { Course } from '~/types/Course'
+import type { Profile } from '~/types/Profile'
+
+export const useCourseEnrollments = () => {
+    const client = useSupabaseClient()
+
+
+    const enrollProfileToCourse = async (profileId: string, courseId: string): Promise<void> => {
+        const { error } = await client.from('course_enrollments').insert({
+            user_id: profileId, // ✅ ici
+            course_id: courseId,
+        })
+
+        if (error) throw error
+    }
+
+    const removeProfileFromCourse = async (profileId: string, courseId: string): Promise<void> => {
+        const { error } = await client
+            .from('course_enrollments')
+            .delete()
+            .match({ user_id: profileId, course_id: courseId }) // ✅ ici aussi
+
+        if (error) throw error
+    }
+
+    const getCoursesForProfile = async (profileId: string): Promise<Course[]> => {
+        const { data, error } = await client
+            .from('course_enrollments')
+            .select('courses(*)')
+            .eq('user_id', profileId) // ✅ ici
+
+        if (error) throw error
+
+        return data.map((row: any) => row.courses)
+    }
+
+    const getProfilesForCourse = async (courseId: string): Promise<Profile[]> => {
+        const { data, error } = await client
+            .from('course_enrollments')
+            .select('profiles(*)')
+            .eq('course_id', courseId)
+
+        if (error) throw error
+
+        return data.map((row: any) => row.profiles)
+    }
+
+    return {
+        enrollProfileToCourse,
+        removeProfileFromCourse,
+        getCoursesForProfile,
+        getProfilesForCourse
+
+    }
+}
